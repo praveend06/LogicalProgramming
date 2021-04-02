5b657fe655c02a6b5d74feaa172aa332359eacaa
Praveen Deshmukh <praveend06@gmail.com>
2021-04-03 00:03:00 +0530

    Remove Duplicate Elements from an ArrayList using LinkedHashSet & JDK8 Streams

--- src/Algorithms/RemoveDuplicateElement.java
+++ src/Algorithms/RemoveDuplicateElement.java
@@ -3,18 +3,25 @@ package Algorithms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RemoveDuplicateElement {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,1,2,3,4,5,1,2,3));
-		
+		ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 1, 2, 3, 4, 5, 1, 2, 3));
+
+		// 1. LinkedHashSet
 		LinkedHashSet<Integer> uniqueNumbers = new LinkedHashSet<Integer>(numbers);
-		
+
 		ArrayList<Integer> numbersListWithoutduplicate = new ArrayList<>(uniqueNumbers);
-		
+
 		System.out.println(numbersListWithoutduplicate);
 
+		// 2. Stream
+
+		List<Integer> collect = numbers.stream().distinct().collect(Collectors.toList());
+		System.out.println(collect);
 	}
 
 }
